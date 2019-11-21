608e586f80dd11bb3ae55bcf57926b1e4df26331
Björn Hallström <bjorn.hallstrom@skane.se>
2019-11-21 17:25:35 +0000

    Allow searching all genes regardless of clinical/research

--- scout/server/blueprints/variants/views.py
+++ scout/server/blueprints/variants/views.py
@@ -104,8 +104,8 @@ def variants(institute_id, case_name):
                     hgnc_symbols.append(hgnc_gene['hgnc_symbol'])
             elif sum(1 for i in store.hgnc_genes(hgnc_symbol)) == 0:
                   not_found_symbols.append(hgnc_symbol)
-            elif is_clinical and (hgnc_symbol not in clinical_symbols):
-                 non_clinical_symbols.append(hgnc_symbol)
+            #elif is_clinical and (hgnc_symbol not in clinical_symbols): # REMOVED TO ALLOW SEARCHING ALL GENES
+            #     non_clinical_symbols.append(hgnc_symbol)
             else:
                 hgnc_symbols.append(hgnc_symbol)
 
